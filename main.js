404a4c5f27d0d54618cc6bee71fbfd86e7eff603
prsG20 <m22cs057@iitj.ac.in>
2023-11-30 20:10:03 +0530

    init

---
+++ main.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client()
+
+const prefix = '!'
+
+client.once('ready', () =>{
+    console.log("Mute Bot Online")
+})
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return
+
+
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
+
+
+    if(command === 'mute-all')
+    if (message.member.voice.channel) {
+        let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+        for (const [memberID, member] of channel.members) {
+          // if (member != message.member)
+      
+          member.voice.setMute(true);
+        }
+        message.channel.send("muted everyone")
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+
+
+
+    if(command === 'unmute-all')
+    if (message.member.voice.channel) {
+        let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+        for (const [memberID, member] of channel.members) {
+            // if (member != message.member)
+        
+            member.voice.setMute(false);
+        }
+        message.channel.send("unmuted everyone")
+        } else {
+          message.reply('You need to join a voice channel first!');
+        }
+})
+
+
+client.login('NzU5MDk4NDkwNTE2MTQ0MTI4.X24jjw.YhStc_aJCCP4Z-tSnE1HzVsjQVc')
